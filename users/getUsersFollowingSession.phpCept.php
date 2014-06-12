5f89ea6ca59d8e61792fcf3903354bdd9bf6ab84
mogetutu <imogetutu@gmail.com>
2014-06-12 10:50:14 +0300

    added tests for users following - session

---
+++ users/getUsersFollowingSession.phpCept.php
@@ -0,0 +1,62 @@
+<?php
+$I = new UserGuy($scenario);
+$I->wantTo('test get request against the /user/:user_id/follows end-point with apikey & session');
+$I->haveHttpHeader('Content-Type', 'text/html');
+$I->haveHttpHeader('User-Agent', 'Api Test/0.1');
+
+$I->logInByUsername(GOOD_USERNAME,GOOD_PASSWORD,$I->api_key_for_crowdmap(LOGIN_URL,'POST'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$session = $I->getSession();
+
+$I->sendGet('/users/7977/follows/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/7977/follows/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => true));
+$resp = $I->grabResponse();
+$I->checkMapObjs($resp, array(4981, 4982));
+
+$I->sendGet('/users/9b6f57db48ecc5ae4fab0c5c990143f7a241db380288011523b26c398137bf83773dca2f08b4344363d98e4ed6d31741bbd26eb2da296ad75263e9ba1090fca8/follows/',
+  array('apikey'=> (string) $I->api_key_for_crowdmap('/users/9b6f57db48ecc5ae4fab0c5c990143f7a241db380288011523b26c398137bf83773dca2f08b4344363d98e4ed6d31741bbd26eb2da296ad75263e9ba1090fca8/follows/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => true));
+$resp = $I->grabResponse();
+$I->checkMapObjs($resp, array(4981, 4982));
+
+$I->sendGet('/users/sarahmorden/follows/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/sarahmorden/follows/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => true));
+$resp = $I->grabResponse();
+$I->checkMapObjs($resp, array(4981, 4982));
+
+$I->sendGet('/users/sarahmorden/follows/4981/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/sarahmorden/follows/4981/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => true));
+$resp = $I->grabResponse();
+$I->checkMapObjs($resp, array(4981));
+
+$I->sendGet('/users/sarahmorden/follows/4983/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/sarahmorden/follows/4983/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => false));
+
+$I->sendGet('/users/3436/follows/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/3436/follows/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => true));
+$I->seeResponseContainsJson(array('maps' => array()));
+
+$I->sendGet('/users/3436/follows/11234/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/3436/follows/11234/', 'GET'), 'session'=> (string) $session));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson(array('status' => 200));
+$I->seeResponseContainsJson(array('success' => false));
