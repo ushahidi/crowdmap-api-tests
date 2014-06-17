6233eb24b595a343be3784d1ea95e755d0b0bd6f
mogetutu <imogetutu@gmail.com>
2014-06-17 17:40:49 +0300

    updated expected json response

--- users/getUsersFollowersSession.phpCept.php
+++ users/getUsersFollowersSession.phpCept.php
@@ -51,10 +51,4 @@ $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
-$I->seeResponseContainsJson(array('maps' => array()));
-
-$I->sendGet('/users/3436/followers/11234/', array('apikey'=> (string) $I->api_key_for_crowdmap('/users/3436/followers/11234/', 'GET'), 'session'=> (string) $session));
-$I->seeResponseCodeIs(200);
-$I->seeResponseIsJson();
-$I->seeResponseContainsJson(array('status' => 200));
-$I->seeResponseContainsJson(array('success' => false));
+$I->seeResponseContainsJson(array('users' => array()));
