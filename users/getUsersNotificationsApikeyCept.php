49e51b8bbe0bef9b28d3ba8a9cf79f9ed04a0c1c
mogetutu <imogetutu@gmail.com>
2014-06-17 17:58:01 +0300

    updated notifications endpoint test

--- users/getUsersNotificationsApikeyCept.php
+++ users/getUsersNotificationsApikeyCept.php
@@ -9,14 +9,12 @@ $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
-$resp = $I->grabResponse();
-$I->checkUserObjs($resp, array(13016, 13015));
 
 // testing count with user_id
 $I->sendGet('/users/7968/notifications', array('count' => TRUE, 'apikey'=> (string) $I->api_key_for_crowdmap('/users/7968/notifications', 'GET'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(array('count' => 2));
+$I->seeResponseContainsJson(array('count' => 0));
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
 
@@ -27,14 +25,13 @@ $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
-$resp = $I->grabResponse();
-$I->checkUserObjs($resp, array(13016, 13015));
+$I->seeResponseContainsJson(array('notifications' => array()));
 
 // testing count request with crowdmapID
 $I->sendGet('/users/53c6c39cf7e29248e0503f7483d4269277e4bf3c2c1e6bfbff1478d441b00212de5267fb17643461d9b99bc7ff4ef044ad64c1dd4b219aaa6884a0022a8d167c/notifications', array('count' => TRUE, 'apikey'=> (string) $I->api_key_for_crowdmap('/users/53c6c39cf7e29248e0503f7483d4269277e4bf3c2c1e6bfbff1478d441b00212de5267fb17643461d9b99bc7ff4ef044ad64c1dd4b219aaa6884a0022a8d167c/notifications', 'GET'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(array('count' => 2));
+$I->seeResponseContainsJson(array('count' => 0));
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
 
@@ -44,14 +41,14 @@ $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
-$resp = $I->grabResponse();
-$I->checkUserObjs($resp, array(13016, 13015));
+$I->seeResponseContainsJson(array('notifications' => array()));
+
 
 // testing count request with username
 $I->sendGet('/users/dmastermind/notifications', array('count' => TRUE, 'apikey'=> (string) $I->api_key_for_crowdmap('/users/dmastermind/notifications', 'GET'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseContainsJson(array('count' => 2));
+$I->seeResponseContainsJson(array('count' => 0));
 $I->seeResponseContainsJson(array('status' => 200));
 $I->seeResponseContainsJson(array('success' => true));
 
