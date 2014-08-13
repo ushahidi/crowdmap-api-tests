da658411be845a32dc2b924e2caad96e27d0af2a
mogetutu <imogetutu@gmail.com>
2014-08-13 13:35:38 +0300

    Updated test for creating new Post

--- posts/postPostsApikeyCept.php
+++ posts/postPostsApikeyCept.php
@@ -5,15 +5,8 @@ $I->haveHttpHeader('Content-Type', 'text/html');
 $I->haveHttpHeader('User-Agent', 'Api Test/0.1');
 
 //test request to update a post
-$I->sendPost('/posts', array('apikey'=> (string) $I->api_key_for_crowdmap('/posts', 'POST')));
-$I->seeResponseCodeIs(401);
-$I->seeResponseIsJson();
-$I->seeResponseContainsJson(array('status' => 401));
-$I->seeResponseContainsJson(array('success' => false));
-$I->seeResponseContainsJson(array('error' => 'This action requires a valid session.'));
-
-//test request to update a different post
-$I->sendPost('/posts', array('apikey'=> (string) $I->api_key_for_crowdmap('/posts', 'POST')));
+$I->sendPost('/posts', array('apikey'=> (string) $I->api_key_for_crowdmap('/posts', 'POST'),
+  'message' => 'This is a message on a post'));
 $I->seeResponseCodeIs(401);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 401));
--- posts/postPostsUnauthenticatedCept.php
+++ posts/postPostsUnauthenticatedCept.php
@@ -5,15 +5,7 @@ $I->haveHttpHeader('Content-Type', 'text/html');
 $I->haveHttpHeader('User-Agent', 'Api Test/0.1');
 
 //test request to update a post
-$I->sendPost('/posts/');
-$I->seeResponseCodeIs(401);
-$I->seeResponseIsJson();
-$I->seeResponseContainsJson(array('status' => 401));
-$I->seeResponseContainsJson(array('success' => false));
-$I->seeResponseContainsJson(array('error' => 'This action requires an API token or a valid session.'));
-
-//test request to update a different post
-$I->sendPost('/posts/');
+$I->sendPost('/posts/', array('message' => 'This is a message on a post.'));
 $I->seeResponseCodeIs(401);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 401));
