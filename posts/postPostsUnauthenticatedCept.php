3cf8675d3ed1ee3accbca8b76deffb413a1e7d38
mogetutu <imogetutu@gmail.com>
2014-08-13 13:11:03 +0300

    fixed post endpoint

--- posts/postPostsUnauthenticatedCept.php
+++ posts/postPostsUnauthenticatedCept.php
@@ -5,7 +5,7 @@ $I->haveHttpHeader('Content-Type', 'text/html');
 $I->haveHttpHeader('User-Agent', 'Api Test/0.1');
 
 //test request to update a post
-$I->sendPost('/posts/11626');
+$I->sendPost('/posts/');
 $I->seeResponseCodeIs(401);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 401));
@@ -13,7 +13,7 @@ $I->seeResponseContainsJson(array('success' => false));
 $I->seeResponseContainsJson(array('error' => 'This action requires an API token or a valid session.'));
 
 //test request to update a different post
-$I->sendPost('/posts/2468');
+$I->sendPost('/posts/');
 $I->seeResponseCodeIs(401);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson(array('status' => 401));
